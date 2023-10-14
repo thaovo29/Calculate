df3684d385e61d6de6caf7078b764828656f7d8f
thaovo29 <thaovo2962002@gmail.com>
2023-10-14 20:16:50 +0700

    des

---
+++ build/Calculus.build/Release-iphoneos/Calculus.build/DerivedSources/CoreDataGenerated/Calculus/Calculus+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  Calculus+CoreDataModel.swift
+//  
+//
+//  Created by Thao Vo on 14/10/2023.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ build/Calculus.build/Release-iphoneos/Calculus.build/DerivedSources/CoreDataGenerated/Calculus/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  
+//
+//  Created by Thao Vo on 14/10/2023.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
